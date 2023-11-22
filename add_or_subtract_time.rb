616e6be3be976eb46cb71e6a3754d19cbb75f498
Namima1 <129132573+Namima1@users.noreply.github.com>
2023-11-22 12:39:10 -0500

    Add files via upload

---
+++ add_or_subtract_time.rb
@@ -0,0 +1,10 @@
+start_of_year = Time.new(2025, 1, 1)
+puts start_of_year 
+
+puts start_of_year + 60 #this is saying add 60 seconds after the start of the year
+
+puts start_of_year + (60 * 60) #this is adding 1 hour. 60 seconds * 60 minutes 
+
+puts start_of_year + (60 * 60 * 24) #this is multiplying by 24 hours so it will give the next day
+
+puts start_of_year - (60 * 60 * 24) # subtraction will take you back 
